c00bea49d1be18396f4091b7b4b130fdd4ad726d
xXTHUND3RXx <fd192168@gmail.com>
2024-09-25 15:46:23 -0300

    continuando a logica do botao mapa

--- back/paginainicial.js
+++ back/paginainicial.js
@@ -1,5 +1,9 @@
-function  clicar(){
-    
-
+function clicar() {
+    const mapaDiv = document.getElementById('mapa');
+    if (mapaDiv.style.display === 'none') {
+        mapaDiv.style.display = 'block'; // Ou 'flex', dependendo do que você deseja
+    } else {
+        mapaDiv.style.display = 'none';
+    }
 }
 
