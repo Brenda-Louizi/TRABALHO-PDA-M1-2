a465450ea6e59d375ee2e628549455cedcbe61f4
hadiel7 <0000124941507XSP@al.educacao.sp.gov.br>
2024-09-26 20:42:47 -0300

    terminei de adicionar todas as perguntas do quiz.

--- back/perguntas-quiz.js
+++ back/perguntas-quiz.js
@@ -49,6 +49,16 @@ const facil = [
         ],
         
     },
+    { 
+        pergunta: "o que o mob creeper de minecraft éra pra ser de começo?" ,
+        resposta: [
+            {texta:"A) Vaca " ,correto:false},
+            {texto:"B) Vilager Zumbi " ,correto:false},
+            {texto:"C) Porco Mutante " ,correto:true},
+            {texto:"D) Enderman " ,correto:false},
+        ]
+
+    }
 ]
 const media = [
     {
@@ -60,45 +70,109 @@ const media = [
         {texto:"D) No mundo dos mortos.", correto:false},
     ],
     
-},
-{
-    pergunta: "Qual é o nome do primeiro chefe que os jogadores geralmente enfrentam em Terraria?",
-    resposta: [
-        {texto:"A) King Slime ",correto:false},
-        {texto:"B) Eye of Cthulhu. ", correto:true},
-        {texto:"C) Skeletron.", correto:false},
-        {texto:"D) Wall of Flesh.", correto:false},
-    ],
-    
-},
-{
-    pergunta: "Média em qual versão de mortal Kombat, o Baraka teve sua primeira aparição?",
-    resposta: [
-        {texto:"A) Mortal Kombat II. ",correto:true},
-        {texto:"B) Mortal Kombat III. ", correto:false},
-        {texto:"C) Mortal Kombat xI.", correto:false},
-        {texto:"D) Baraka está em todas as versões.", correto:false},
-    ],
-    
-},
-{
-    pergunta: "Qual o nome da área do mapa que era conhecida por suas grandes construções em Fortnite Capítulo 1?",
-    resposta: [
-        {texto:"A) Torres Tortas. ",correto:true},
-        {texto:"B) Alameda Arborizada. ", correto:false},
-        {texto:"C) Parque Agradável.", correto:false},
-        {texto:"D) Via do Varejo.", correto:false},
-    ],
-    
-},
-{
-    pergunta: "Qualquer super herói que apareceu no como comando Tony halk pro skater 2?",
-    resposta: [
-        {texto:"A) Batman. ",correto:false},
-        {texto:"B) Super homem. ", correto:false},
-        {texto:"C) Homem aranha .", correto:true},
-        {texto:"D) Capitão América.", correto:false},
-    ],
-    
-},
+    },
+    {
+        pergunta: "Qual é o nome do primeiro chefe que os jogadores geralmente enfrentam em Terraria?",
+        resposta: [
+            {texto:"A) King Slime ",correto:false},
+            {texto:"B) Eye of Cthulhu. ", correto:true},
+            {texto:"C) Skeletron.", correto:false},
+            {texto:"D) Wall of Flesh.", correto:false},
+        ],
+        
+    },
+    {
+        pergunta: "Média em qual versão de mortal Kombat, o Baraka teve sua primeira aparição?",
+        resposta: [
+            {texto:"A) Mortal Kombat II. ",correto:true},
+            {texto:"B) Mortal Kombat III. ", correto:false},
+            {texto:"C) Mortal Kombat xI.", correto:false},
+            {texto:"D) Baraka está em todas as versões.", correto:false},
+        ],
+        
+    },
+    {
+        pergunta: "Qual o nome da área do mapa que era conhecida por suas grandes construções em Fortnite Capítulo 1?",
+        resposta: [
+            {texto:"A) Torres Tortas. ",correto:true},
+            {texto:"B) Alameda Arborizada. ", correto:false},
+            {texto:"C) Parque Agradável.", correto:false},
+            {texto:"D) Via do Varejo.", correto:false},
+        ],
+        
+    },
+    {
+        pergunta: "Qualquer super herói que apareceu no como comando Tony halk pro skater 2?",
+        resposta: [
+            {texto:"A) Batman. ",correto:false},
+            {texto:"B) Super homem. ", correto:false},
+            {texto:"C) Homem aranha .", correto:true},
+            {texto:"D) Capitão América.", correto:false},
+        ],
+    },
+    {
+        pergunta: "qual é o mob que ganhou a votação de 2023?" ,
+        resposta: [
+            {texto:"A) Allay ", correto:false},
+            {texto:"B) Tatu ", correto:true},
+            {texto:"C) Carangueijo ", correto:false},
+            {texto:"D) Pinguin ", correto:false},
+        ]
+    }
+]
+const dificil =[
+    {
+        pergunta: "Qual o nome da mãe de Zeus?" ,
+        resposta: [
+            {texto:"A) Gaia ", correto:false},
+            {texto:"B) Láquesis ", correto:false},
+            {texto:"C) Cloto ", correto:false},
+            {texto:"D) Reia ", correto:true},
+        ]
+    },
+    {
+        pergunta: "Qual é o nome do evento que é desencadeado ao usar a 'Suspicious Looking Skull'?" ,
+        resposta: [
+            {texto:"A) Pumpkin Moon ", correto:false},
+            {texto:"B) Frost Moon ", correto:false},
+            {texto:"C) Solar Eclipse ", correto:false},
+            {texto:"D) Old One's Army ", correto:true},
+        ]
+    },
+    {  
+        pergunta: "Qual personagem de street fighter é brasileiro?" ,
+        resposta: [
+            {texto:"A) Blanka ", correto:false},
+            {texto:"B) Laura Matsuda ", correto:false},
+            {texto:"C) Chun-Li ", correto:false},
+            {texto:"D) Ryu ", correto:true},
+        ]
+    },
+    {  
+        pergunta: "Qual é o número máximo de máteriais(madeira, pedra, metal) que um jogador pode carregar de cada tipo em Fortnite?" ,
+        resposta: [
+            {texto:"A) 750 ", correto:false},
+            {texto:"B) 1000 ", correto:false},
+            {texto:"C) 500 ", correto:true},
+            {texto:"D) 999 ", correto:false},
+        ]
+    },
+    {  
+        pergunta: "Qual jogo deu origem a Counter-Strike?" ,
+        resposta: [
+            {texto:"A) GTA San Andreas ", correto:false},
+            {texto:"B) Gmode ", correto:false},
+            {texto:"C) Battlefield 2 ", correto:false},
+            {texto:"D) Half-Life ", correto:true},
+        ]
+    },
+    {  
+        pergunta: "Qual foi a data de lançamento do Minecraft?" ,
+        resposta: [
+            {texto:"A) 18 de novembro de 2011 ", correto:true},
+            {texto:"B) 21 de Julho de 2011 ", correto:false},
+            {texto:"C) 1 de Setembro de 2011 ", correto:false},
+            {texto:"D) 15 de Julho de 2011 ", correto:false},
+        ] 
+    }
 ]
