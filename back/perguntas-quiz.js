5c7979ec21d8040032bd9392f88c0f8e2bdf88b1
odilonskt <odilon123c@gmail.com>
2024-09-26 16:00:09 -0300

    adicionei arry das questão facil e media

--- back/perguntas-quiz.js
+++ back/perguntas-quiz.js
@@ -1,7 +1,104 @@
 const facil = [
     {
-        pergunta: "",
-        resposta: "",
+        pergunta: "Quando kratos se torna o novo deus da guerra?",
+        resposta: [
+            {texto:"A-depois de matar atena.",correto:false},
+            {texto:"B-depos de matar zeus.", correto:true},
+            {texto:"C-depois de matar Ares.", correto:false},
+            {texto:"D-depois de matar Átropos.", correto:false},
+        ],
         
-    }
+    },
+    {
+        pergunta: "Qual material é necessário para criar a armadura de Meteorite?",
+        resposta: [
+            {texto:"A) Hellstone",correto:false},
+            {texto:"B) Demonite", correto:false},
+            {texto:"C) Meteorite", correto:true},
+            {texto:"D) Crimtane.", correto:false},
+        ],
+        
+    },
+    {
+        pergunta: "como pedir alguém em namoro no stardew valley",
+        resposta: [
+            {texto:"A) dando um anel ",correto:false},
+            {texto:"B) dando uma rosa ", correto:false},
+            {texto:"C) dando um buquê especial", correto:true},
+            {texto:"D) dando um anel especial", correto:false},
+        ],
+        
+    },
+    {
+        pergunta: " Em que ano o jogo Fortnite foi lançado?",
+        resposta: [
+            {texto:"A)  2014 ",correto:false},
+            {texto:"B) 2017 ", correto:true},
+            {texto:"C) 2019", correto:false},
+            {texto:"D) 2021", correto:false},
+        ],
+        
+    },
+    {
+        pergunta: "Qual cidade de runeterra que o teemo deu sua origem?",
+        resposta: [
+            {texto:"A) Bandópolis ",correto:true},
+            {texto:"B) Demacia ", correto:false},
+            {texto:"C) Ionia", correto:false},
+            {texto:"D) O Vazio", correto:false},
+        ],
+        
+    },
+]
+const media = [
+    {
+    pergunta: "Como Kratos perde sua divindide?",
+    resposta: [
+        {texto:"A) Na luta contra Ares. ",correto:false},
+        {texto:"B) Lutando contra as Moiras. ", correto:false},
+        {texto:"C) Colocando-a na espada de Zeus.", correto:true},
+        {texto:"D) No mundo dos mortos.", correto:false},
+    ],
+    
+},
+{
+    pergunta: "Qual é o nome do primeiro chefe que os jogadores geralmente enfrentam em Terraria?",
+    resposta: [
+        {texto:"A) King Slime ",correto:false},
+        {texto:"B) Eye of Cthulhu. ", correto:true},
+        {texto:"C) Skeletron.", correto:false},
+        {texto:"D) Wall of Flesh.", correto:false},
+    ],
+    
+},
+{
+    pergunta: "Média em qual versão de mortal Kombat, o Baraka teve sua primeira aparição?",
+    resposta: [
+        {texto:"A) Mortal Kombat II. ",correto:true},
+        {texto:"B) Mortal Kombat III. ", correto:false},
+        {texto:"C) Mortal Kombat xI.", correto:false},
+        {texto:"D) Baraka está em todas as versões.", correto:false},
+    ],
+    
+},
+{
+    pergunta: "Qual o nome da área do mapa que era conhecida por suas grandes construções em Fortnite Capítulo 1?",
+    resposta: [
+        {texto:"A) Torres Tortas. ",correto:true},
+        {texto:"B) Alameda Arborizada. ", correto:false},
+        {texto:"C) Parque Agradável.", correto:false},
+        {texto:"D) Via do Varejo.", correto:false},
+    ],
+    
+},
+{
+    pergunta: "Qualquer super herói que apareceu no como comando Tony halk pro skater 2?",
+    resposta: [
+        {texto:"A) Batman. ",correto:false},
+        {texto:"B) Super homem. ", correto:false},
+        {texto:"C) Homem aranha .", correto:true},
+        {texto:"D) Capitão América.", correto:false},
+    ],
+    
+},
 ]
